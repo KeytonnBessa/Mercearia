f7780a3dc79bcacb2ec3d001aee7c3983269cb62
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2023-10-24 08:36:23 -0300

    Criação ProdutoBLL, ClienteBLL
    Métodos implementados também

---
+++ GestaoComercial/BLL/ClienteBLL.cs
@@ -0,0 +1,38 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ClienteBLL
+    {
+        public void Inserir(Cliente _cliente)
+        {
+            if (string.IsNullOrEmpty(_cliente.Nome))
+                throw new Exception("Informe o nome do Cliente.");
+
+            new ClienteDAL().Inserir(_cliente);
+        }
+        public void Alterar(Cliente _cliente)
+        {
+            new ClienteDAL().Alterar(_cliente);
+        }
+        public void Excluir(int _id)
+        {
+            new ClienteDAL().Excluir(_id);
+        }
+        public List<Cliente> BuscarTodos()
+        {
+            return new ClienteDAL().BuscarTodos();
+        }
+        public Cliente BuscarPorId(int _id)
+        {
+            return new ClienteDAL().BuscarPorId(_id);
+        }
+
+    }
+}
---
+++ GestaoComercial/BLL/ProdutoBLL.cs
@@ -0,0 +1,38 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ProdutoBLL
+    {
+        public void Inserir(Produto _produto)
+        {
+            if (string.IsNullOrEmpty(_produto.Nome))
+                throw new Exception("Informe o nome do produto.");
+
+            new ProdutoDAL().Inserir(_produto);
+        }
+        public void Alterar(Produto _produto)
+        {
+            new ProdutoDAL().Alterar(_produto);
+        }
+        public void Excluir(int _id)
+        {
+            new ProdutoDAL().Excluir(_id);
+        }
+        public List<Produto> BuscarTodos()
+        {
+            return new ProdutoDAL().BuscarTodos();
+        }
+        public Produto BuscarPorId(int _id)
+        {
+            return new ProdutoDAL().BuscarPorId(_id);
+        }
+
+    }
+}
