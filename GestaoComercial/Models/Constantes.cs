91683f219c68fd745e499fe476b7108be838ead4
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2023-10-24 11:31:16 -0300

    Criação do formulario que puxa os dados do banco

--- GestaoComercial/Models/Constantes.cs
+++ GestaoComercial/Models/Constantes.cs
@@ -8,7 +8,7 @@ namespace Models
 {
     public static class Constantes
     {
-        private static string stringDeConexao = "User ID=SA;Initial Catalog=Mercearia,Data Source=.\\sqlexpress2019;Password=Senailab02";
+        private static string stringDeConexao = "User ID=SA;Initial Catalog=Mercearia;Data Source=.\\sqlexpress2019;Password=Senailab02";
         public static string StringDeConexao 
         {
             get
---
+++ GestaoComercial/UIWinFormsApp/FormBuscarUsuario.cs
@@ -0,0 +1,33 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIWinFormsApp
+{
+    public partial class FormBuscarUsuario : Form
+    {
+        public FormBuscarUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bindingSourceUsuario.DataSource = new UsuarioBLL().BuscarTodos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
